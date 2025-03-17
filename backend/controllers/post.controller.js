b6aae052a4d8dab45456c0c43cddcfbe89755f48
Haupt04 <116957227+Haupt04@users.noreply.github.com>
2025-03-17 15:55:34 +0200

    Notification Page Done

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -104,6 +104,7 @@ export const likeUnlikePost = async (req, res) => {
 			// Unlike post
 			await Post.updateOne({ _id: postId }, { $pull: { likes: userId } });
 			await User.updateOne({ _id: userId }, { $pull: { likedPosts: postId } });
+			
 
 			const updatedLikes = post.likes.filter((id) => id.toString() !== userId.toString());
 			res.status(200).json(updatedLikes);
